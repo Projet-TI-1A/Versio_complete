c3dae33f9a5619e51b218e63cae2d9b131580d08
Emma-dupont <57994890+Emma-dupont@users.noreply.github.com>
2020-04-02 18:45:46 +0200

    Add files via upload

---
+++ Fin.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Fin.h"
+#include "resultat.h"
+
+
+using namespace sf;
+using namespace std;
+
+
+void affichage_fin(RenderWindow& window, int& gamemode, int erreurs, int points){
+	
+	Event event;
+	while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				{window.close();}
+			
+			if (event.type == Event::KeyPressed)
+			{
+				if ((event.key.code == Keyboard::Q) or (event.key.code == Keyboard::N))
+				{
+					window.close();
+				}
+			}
+		}
+
+	affichage_erreur(points, erreurs, window);
+	
+}
---
+++ Fin.h
@@ -0,0 +1,7 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+void affichage_fin(RenderWindow& window, int& gamemode, int erreurs, int points);
