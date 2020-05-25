5c67b429e6ab902c69b2ddfded4ccb8936d37b07
Emma-dupont <57994890+Emma-dupont@users.noreply.github.com>
2020-05-25 18:03:29 +0200

    Update Jeu.h

--- Jeu.h
+++ Jeu.h
@@ -12,11 +12,18 @@
 #ifndef JEU_H
 #define JEU_H
 
+/**************Ce fichier contient le coeur de la simulation: la dissection de la tumeur à travers les différentes zones**************/
+
+//simulation de la dissection
 void init_jeu(RenderWindow&, int& ,int& ,int& ,int& ,int& ,int& ,int& ,int& ,int* ,int& , int*, int*,Texture& ,Sprite& ,ligne& ,ligne& , int, int&);
 
+//Initialisation des variables necessaires pour la simulation
 void simulation(RenderWindow&, int&, int&, int&, int&, int&, int&, int&, int&, int*, int&, int*,int*, tabpoint&, tabpoint&, int**, Texture&, Sprite&, point&, ligne& , ligne& , int&, time_t*, int, int, int, int);
 
+//fonction permettant de recomencer la simulation
 void restart(tabpoint&, tabpoint&, int&, time_t&, ligne&, ligne&,int&, int, int, int , int**);
 
+//initialisation des paramètres réglables par l'utilisateur via le menu
 void Initpara(int&, int&, int&, int&);
+
 #endif
