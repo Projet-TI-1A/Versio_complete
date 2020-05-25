e36f9ec551f80e616ceed748a390581ccaa766c2
Emma-dupont <57994890+Emma-dupont@users.noreply.github.com>
2020-05-25 19:27:22 +0200

    Update toucher.cpp

--- toucher.cpp
+++ toucher.cpp
@@ -5,12 +5,13 @@
 #include <ctime>
 #include <time.h>
 
-
-
 using namespace std;
 using namespace sf;
 
-void Position_Curseur(int& x, int& y, RenderWindow& window)/*Récupère la postion de la souris relative à la fenêtre*/
+/************ Ce fichier contient les fonction qui récupèrent et traitent les données récupérées de l'écran*************/
+
+//Récupère la postion de la souris relative à la fenêtre
+void Position_Curseur(int& x, int& y, RenderWindow& window)
 {
 	Vector2i localPosition = Mouse::getPosition(window);
 	x = localPosition.x;
@@ -18,6 +19,8 @@ void Position_Curseur(int& x, int& y, RenderWindow& window)/*Récupère la posti
 }
 
 /*******************************************************************************/
+
+//Ajoute un point au tableau qui collecte les points de l'écran touchés (tableau comptant les erreuurs ou les points corrects)
 
 void Ajoute_point(vector<point> tab_point, int* taille, RenderWindow& window)
 {
@@ -31,20 +34,9 @@ void Ajoute_point(vector<point> tab_point, int* taille, RenderWindow& window)
 	tab_point.push_back(p1);
 }
 
-/*********************************************************************************/
+/*******************fonctions de la classe point**************************************************************/
 
-void point::set(int x, int y)
-{
-	this->x=x;
-	this->y=y;
-}
-
-void point::get(int& x, int& y)
-{
-	x=this->x;
-	y=this->y;
-}
-
+//constructeur
 point::point(){}
 
 point::point(int x, int y)
@@ -53,16 +45,34 @@ point::point(int x, int y)
 	this->y=y;
 }
 
-/*******************************************************************************/
+//implemente des valeurs à un point
+void point::set(int x, int y)
+{
+	this->x=x;
+	this->y=y;
+}
 
+//permet acceder aux coordonnées d'un point
+void point::get(int& x, int& y)
+{
+	x=this->x;
+	y=this->y;
+}
+
+
+/*************************fonction de la classe tabpoint (tableau comtenant des points)************************************/
+
+//constructeur
 tabpoint::tabpoint(){taille=0;}
 
+//ajoute un point
 void tabpoint::append(point& p)
 {
 	this->tab.push_back(p);
 	taille++;
 }
 
+//donne acces au ième point du tableau
 void tabpoint::get(point& p, int i)
 {
 	int x,y;
@@ -70,28 +80,17 @@ void tabpoint::get(point& p, int i)
 	p.set(x, y);
 }
 
+//donne la taille du tableau
 int tabpoint::gettaille()
 {
 	return(taille);
 }
 
-
-/*****************************************************************************/
-
+//réinitialise le tableau
 void tabpoint::reset()
 {
 	this->tab.clear();
 	this->taille=0;
 }
 
-/********************************************************************************/
-
-
-
-
-
-
-
-
-
-
+/*******************************************************************************/
