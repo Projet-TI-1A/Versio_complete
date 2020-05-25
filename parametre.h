a77715fd044393c8d238d86c6070d2ba9191c36c
texto15 <57966801+texto15@users.noreply.github.com>
2020-05-25 16:07:25 +0200

    Update parametre.h

--- parametre.h
+++ parametre.h
@@ -5,8 +5,8 @@
 //#define R 15 //coté du curseur
 //#define nbzone 3
 #define r 150 //rayon du cercle
-#define L 480 //longeur de l'écran
-#define l 320 //largeur de l'écran
+#define L 800 //longeur de l'écran
+#define l 480 //largeur de l'écran
 #define xcentre 250 //position du centre du cercle
 #define ycentre 170
 //#define condition95 95 //pourcentage minimum a completer pour passer a la zone suivante
