844601fa5aba4b4202cd9bd2185e75a2d64e0f59
Emma-dupont <57994890+Emma-dupont@users.noreply.github.com>
2020-05-25 19:02:12 +0200

    Update Fin.h

--- Fin.h
+++ Fin.h
@@ -4,4 +4,6 @@
 using namespace std;
 using namespace sf;
 
+
+/****** ce fichier contient les fonction permettant d'afficher les données a l'utilisateur à la fin de simulation****/
 void affichage_fin(RenderWindow&,int, int&, int*, int*, int*, time_t*);
