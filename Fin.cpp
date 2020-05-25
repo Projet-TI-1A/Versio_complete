277d68aa8bedfab6d2301632fa2c987ff89166a2
Emma-dupont <57994890+Emma-dupont@users.noreply.github.com>
2020-05-25 19:01:48 +0200

    Update Fin.cpp

--- Fin.cpp
+++ Fin.cpp
@@ -7,6 +7,7 @@
 using namespace sf;
 using namespace std;
 
+/****** ce fichier contient les fonction permettant d'afficher les données a l'utilisateur à la fin de simulation****/
 
 void affichage_fin(RenderWindow& window,int nbzone, int& gamemode, int* erreurs, int* points, int* n, time_t* temps)
 {
