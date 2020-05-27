397bf224bfa48387bfcd55481d3ab936be6f70ba
texto15 <57966801+texto15@users.noreply.github.com>
2020-05-27 21:03:33 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -7,7 +7,7 @@
 using namespace sf;
 using namespace std;
 
-void Grand_cercle(RenderWindow&);
+void Grand_cercle(RenderWindow&, int, int, int);
 void Position_ligne(int ,int, RenderWindow&);
 void Dessine_point(point ,RenderWindow&, int );
 void Dessine_plus_points(tabpoint, tabpoint, RenderWindow&,int );
